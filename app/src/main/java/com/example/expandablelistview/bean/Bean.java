bd5f2c0dfef80d435fab8007b237e8c1d6092335
caozhongbin <861088350@qq.com>
2017-09-21 20:14:01 +0800

    Initial commit

---
+++ app/src/main/java/com/example/expandablelistview/bean/Bean.java
@@ -0,0 +1,32 @@
+package com.example.expandablelistview.bean;
+
+/**
+ * Created by Administrator on 2017/9/21.
+ */
+
+public class Bean {
+
+    private String price;
+    private String number;
+
+    public Bean(String price, String number) {
+        this.price = price;
+        this.number = number;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
